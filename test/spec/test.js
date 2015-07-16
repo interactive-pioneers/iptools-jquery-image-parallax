65bc86131b6015b40ea8c1636ab9359dddefd5b2
Georg Meyer <gm@interactive-pioneers.de>
2015-07-16 12:52:59 +0200

    Add basic tests. Fix #2

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,14 +3,20 @@
   'use strict';
 
   describe('IPTImageParallax', function() {
+
     var config = {};
     var pluginName = 'plugin_iptImageParallax';
+    var selector = '.image-parallax';
     var $object = null;
 
     describe('init', function() {
 
       beforeEach(function() {
-        $object = $('.image-parallax').iptImageParallax(config);
+        $object = $(selector).iptImageParallax(config);
+      });
+
+      afterEach(function() {
+        $object.data(pluginName).destroy();
       });
 
       it('expected to construct object', function() {
@@ -19,10 +25,26 @@
 
     });
 
+    describe('updateViewport', function() {
+
+      beforeEach(function() {
+        $object = $(selector).iptImageParallax(config);
+      });
+
+      afterEach(function() {
+        $object.data(pluginName).destroy();
+      });
+
+      it('expected to set/update backgroundPosition', function() {
+        return expect($object.css('backgroundPosition')).to.match(/^[0-9]{0,2}.%\s[0-9]{0,2}.%$/);
+      });
+
+    });
+
     describe('destroy', function() {
 
       beforeEach(function() {
-        $object = $('.image-parallax').iptImageParallax(config);
+        $object = $(selector).iptImageParallax(config);
       });
 
       it('expected to remove data', function() {
@@ -31,6 +53,7 @@
       });
 
     });
+
   });
 
 })();
